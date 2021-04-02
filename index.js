6ab68e68e5c5ff2c0f7e57db60f0ba02e3522443
jonathan <jhector39@gmail.com>
2021-04-02 00:37:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(somevalue){
+  let result;
+  switch(somevalue){
+    case 199:
+    result =  `This one is on me!`;
+    break;
+    case 2001:
+    result = `I will gladly take your thirty bucks.`;
+    break;
+    case 2501:
+    result = `No can do.`;  
+    }
+    return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(somecity){
+  let apple;
+  switch(somecity){
+    case `NYC`:
+    apple = `Ok, sounds good.`;
+    break;
+    case `Pittsburgh`:
+    apple = `No go.`;
+  }
+  return apple;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(string){
+  let message;
+  switch(string){
+    case 'generous':
+      message = `Thank you so much.`;
+      break;
+    case `not as generous`: 
+      message =`Thank you.`;
+      break;
+    default:
+      message = `Bye.`;     
+  }
+  return message;
 }
